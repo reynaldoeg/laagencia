05c6db4b3785b4b4bbc020208e61fc63656f496b
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2018-04-07 23:11:55 -0500

    Add Item Migration

---
+++ database/migrations/2018_04_08_023601_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->double('quantity', 15, 8);
+            $table->double('total', 15, 8);
+            $table->timestamps();
+        });
+
+        Schema::table('items', function(Blueprint $table) {
+           $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
